86da46b16012bf8bb0fd87c28517616738715a46
Damian Figueroa <damy.mtl@gmail.com>
2023-07-13 02:13:10 -0300

    feat: frontend added with simple style

--- api/src/app.js
+++ api/src/app.js
@@ -1,9 +1,11 @@
 import express from "express";
 import clientsRoutes from './routes/client.routes.js'
+import cors from 'cors'
 
 const app = express()
 
 //middlewares 
+app.use(cors())
 app.use(express.json())
 app.use(clientsRoutes)
 
